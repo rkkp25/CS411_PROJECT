c90cc9c1d712411a10034b0d1ea9838f32e95348
Ashton Fox <ashfox@bu.edu>
2023-11-23 21:25:33 -0500

    set up mysql database + implemented database-related functions

--- historicolor/src/app/backend.py
+++ historicolor/src/app/backend.py
@@ -3,18 +3,19 @@ from flask import Flask, Response, request, flask_login, render_template, redire
 import mysql.connector
 import os, base64
 
-host = "your_host"
-user = "your_user"
-password = "your_password"
-database = "your_database"
+mysql = MySQL()
+app = Flask(__name__)
+app.secret_key = 'holy guacamole' #CHANGE THIS TO SOMETHING SECURE
 
-# Establish a connection to the MySQL server
-connection = mysql.connector.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=database
-)
+#these are for database credentials
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'outthewazoo'
+app.config['MYSQL_DATABASE_DB'] = 'historicolor'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
 
 
 #THIS IS TEMPLATE CODE FOR REFERENCE ON HOW TO PULL DATA FROM THE DATABASE
@@ -27,6 +28,15 @@ def calc_score(hex_code):
     return
 
 def display_scores():
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, score FROM scores ORDER BY score LIMIT 10")
+    return cursor.fetchall()
+
+def save_user_score(name, score):
+    assert type(name) == str
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO scores (name, score) VALUES (%s, %s)", (name, score))
+    conn.commit()
     return
 
 def get_image():
@@ -34,9 +44,5 @@ def get_image():
 
 def find_dominant_color(image):
     return
-
-def save_score():
-    return
-
 
 
