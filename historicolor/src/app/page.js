bc2c5065656e56b0ba37690cdb3430849d72a897
Mithat Kus <mthtks@bu.edu>
2023-11-30 16:48:20 -0500

    put the circles on top of the table

--- historicolor/src/app/page.js
+++ historicolor/src/app/page.js
@@ -1,36 +1,47 @@
-import Image from 'next/image'
+import Image from 'next/image';
+
+const Color1 = "#00FF00"; // Green
+const Color2 = "#00FF00"; // Green
+const Color3 = "#00FF00"; // Green
+
+const Circle = ({ color }) => (
+  <div style={{
+    width: '50px',
+    height: '50px',
+    backgroundColor: color,
+    borderRadius: '50%',
+    display: 'inline-block',
+    margin: '10px'
+  }} />
+);
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-
-
-      {/*  HISTORICOLOR LOGO */}
+      {/* HISTORICOLOR LOGO */}
       <div>
-        <div>
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/histori_logo.svg"
-              alt="Historicolor Logo"
-              width={500}
-              height={2}
-              priority
-            />
-          </a>
-        </div>
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/histori_logo.svg"
+            alt="Historicolor Logo"
+            width={500}
+            height={2}
+            priority
+          />
+        </a>
       </div>
 
-      {/* IMAGE FROM API */}
-      {/* this is where we would make a call to the api and then display the image */}
-      <div className="main-component">
-        <div className="image-component">
+      {/* Flex Container for Image and Color Information */}
+      <div className="flex flex-row justify-center items-start w-full">
+        {/* IMAGE FROM API */}
+        <div className="image-component flex flex-col items-center">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="pointer-events-none"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -38,31 +49,33 @@ export default function Home() {
               src="/starry_night.jpg"
               alt="Starry Night"
               width={500}
-              height={2}
+              height={500}
               priority
             />
-        <div className="group rounded-lg border border-black px-5 py-4 transition-colors">
-           Color 1 -- Color 2 -- Color 3 
-          <div className="enter-rbg">
-              C1: Red: blah -- Green: blah -- Blue: blah <br/>
-              C2: Red: blah -- Green: blah -- Blue: blah <br/>
-              C3: Red: blah -- Green: blah -- Blue: blah <br/>
-            
-          </div>
-          <div className="score">
-              This is where the calculated score would be outputed
-          </div>
+          </a>
         </div>
-         
 
-         </a>
+        {/* Color Information Group */}
+        <div className="group rounded-lg border border-black px-5 py-4 transition-colors">
+          {/* Color Circles */}
+          <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
+            <Circle color={Color1} />
+            <Circle color={Color2} />
+            <Circle color={Color3} />
+          </div>
+          
+          {/* Color Information */}
+          <div className="enter-rbg">
+            C1: Red: blah -- Green: blah -- Blue: blah <br/>
+            C2: Red: blah -- Green: blah -- Blue: blah <br/>
+            C3: Red: blah -- Green: blah -- Blue: blah <br/>
+          </div>
+          
+          <div className="score">
+            This is where the calculated score would be outputted
+          </div>
         </div>
       </div>
-
-
-
-
-
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -71,47 +84,46 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className="mb-3 text-2xl font-semibold">
             Scoreboard{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Find in-depth information about Next.js features and API.
           </p>
         </a>
       </div>
 
-
-      {/*  SCOREBOARD */}
+      {/* SCOREBOARD */}
       <table>
-       <tbody>
-        <tr>
-          <th>Initials</th>
-          <th>Score</th>
-        </tr>
-        <tr>
-          <td>Kris</td>
-          <td>K Score</td>
-        </tr>
-        <tr>
-          <td>Valentina</td>
-          <td>V Score</td>
-        </tr>
-        <tr>
-          <td>Ashton</td>
-          <td>A Score</td>
-        </tr>
-        <tr>
-          <td>Mithat</td>
-          <td>M Score</td>
-        </tr>
-        <tr>
-          <td>Amongus</td>
-          <td>A Score</td>
-        </tr>
-       </tbody>
+        <tbody>
+          <tr>
+            <th>Initials</th>
+            <th>Score</th>
+          </tr>
+          <tr>
+            <td>Kris</td>
+            <td>K Score</td>
+          </tr>
+          <tr>
+            <td>Valentina</td>
+            <td>V Score</td>
+          </tr>
+          <tr>
+            <td>Ashton</td>
+            <td>A Score</td>
+          </tr>
+          <tr>
+            <td>Mithat</td>
+            <td>M Score</td>
+          </tr>
+          <tr>
+            <td>Amongus</td>
+            <td>A Score</td>
+          </tr>
+        </tbody>
       </table>
     </main>
   )
