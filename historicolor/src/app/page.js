35a0118204ee7bb36f345aa93f6300fbd9041e6d
kkpeters <kristinekpeters@gmail.com>
2023-11-30 13:44:03 -0500

    trying to add an image placeholder, not working

--- historicolor/src/app/page.js
+++ historicolor/src/app/page.js
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
+
+
+      {/*  HISTORICOLOR LOGO */}
       <div>
         <div>
           <a
@@ -14,7 +17,6 @@ export default function Home() {
             <Image
               src="/histori_logo.svg"
               alt="Historicolor Logo"
-              //className="dark:invert" // this turns the white into black on the logo
               width={500}
               height={2}
               priority
@@ -22,6 +24,26 @@ export default function Home() {
           </a>
         </div>
       </div>
+
+      {/* IMAGE FROM API */}
+      {/* this is where we would make a call to the api and then display the image 
+      <div>
+        <div>
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            target="_blank"
+            rel="noopener noreferrer"
+          </a>
+            <Image 
+              src="starry_night.jpg" 
+              alt="Starry Night">
+              width={500}
+              height={450}
+              priority
+            >
+        </div>
+      </div>
+      */}
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -40,36 +62,38 @@ export default function Home() {
             Find in-depth information about Next.js features and API.
           </p>
         </a>
-
       </div>
-        <table>
-         <tbody>
-          <tr>
-            <th>Initials</th>
-            <th>Score</th>
-          </tr>
-          <tr>
-            <td>Kris</td>
-            <td>K Score</td>
-          </tr>
-          <tr>
-            <td>Valentina</td>
-            <td>V Score</td>
-          </tr>
-          <tr>
-            <td>Ashton</td>
-            <td>A Score</td>
-          </tr>
-          <tr>
-            <td>Mithat</td>
-            <td>M Score</td>
-          </tr>
-          <tr>
-            <td>Amongus</td>
-            <td>A Score</td>
-          </tr>
-         </tbody>
-        </table>
+
+
+      {/*  SCOREBOARD */}
+      <table>
+       <tbody>
+        <tr>
+          <th>Initials</th>
+          <th>Score</th>
+        </tr>
+        <tr>
+          <td>Kris</td>
+          <td>K Score</td>
+        </tr>
+        <tr>
+          <td>Valentina</td>
+          <td>V Score</td>
+        </tr>
+        <tr>
+          <td>Ashton</td>
+          <td>A Score</td>
+        </tr>
+        <tr>
+          <td>Mithat</td>
+          <td>M Score</td>
+        </tr>
+        <tr>
+          <td>Amongus</td>
+          <td>A Score</td>
+        </tr>
+       </tbody>
+      </table>
     </main>
   )
 }
